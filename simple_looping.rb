af65f77c72f07fdfedcf946d1441792b242be8d4
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-11 01:49:18 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times.times do
-    phrase
+    puts phrase
   end
 end
 
